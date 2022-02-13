1f0de9271d7b343bc9684692da2f39ea27b18a5f
Mathieu VALERY <66632538+mathieu-valery@users.noreply.github.com>
2022-02-13 19:38:34 +0100

    normalize value

--- javascript.js
+++ javascript.js
@@ -4,10 +4,13 @@ const buttonBox = button.getBoundingClientRect();
 const centerX = buttonBox.left + buttonBox.width/2 - window.pageXOffset;
 const centerY = buttonBox.top + buttonBox.height/2 - window.pageYOffset;
 let isTurnable = false;
+let value = 0;
 
 function toggleIsTurnable() {
     isTurnable = !isTurnable;
 }
+
+function normalize (val, max, min, newMax) { return ((val - min) / (max - min) * newMax); }
 
 function rotateButton(e) {
     if (isTurnable) {
@@ -19,7 +22,14 @@ function rotateButton(e) {
             degree = 225;
         }
 
+        if (degree >= 225 && degree <= 360) {
+            value = degree - 225;
+        } else if (degree >= 0 && degree <= 180) {
+            value = degree + 135;
+        }
+
         button.style.transform = "rotate("+degree+"deg)";
+        console.log(normalize(value, 360, 0, 1.33))
     }
 }
 
